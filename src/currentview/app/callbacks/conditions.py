f4d4db6ade4e68e61dd2361448dce5ec8777df06
PouriaDan <pouriadan@gmail.com>
2026-02-25 12:05:19 -0500

    Removed debug statement

--- src/currentview/app/callbacks/conditions.py
+++ src/currentview/app/callbacks/conditions.py
@@ -108,8 +108,6 @@ def register_condition_callbacks():
         trigger,
     ):
         """Add a new condition."""
-        print(matched_query_base)
-
         error_messages = _validate_inputs(files, contig, pos)
 
         if error_messages:
